1a9ded7a0a8a4ac279b1dca215fa09345fdcbce0
HisenShowcase <hisencontact@gmail.com>
2025-03-09 00:01:43 +0100

    push-1.0

--- src/app/component/web/navbar/navbar.component.ts
+++ src/app/component/web/navbar/navbar.component.ts
@@ -46,7 +46,7 @@ export class NavbarComponent {
   }
 
   navigateTo(path: string) {
-    window.location.href = `${this.baseUrl}${path}`;
+    window.location.href = `${this.baseUrl}#${path}`;
   }
 
   openExternalLink(url: string) {
